e9310287fe4f707b7ef94d699588e136619419e4
Dimitar.Petroww <Mitko66692@abv.bg>
2021-03-31 20:00:42 +0300

    update edit

--- editJob.php
+++ editJob.php
@@ -1,5 +1,4 @@
 <?php 
-
 $db = new mysqli("localhost", "root", "", "devriX");
 $result = $db -> query("UPDATE jobs SET title = '$_POST[title]', description = '$_POST[description]', company = '$_POST[company]', salary = '$_POST[salary]' WHERE id = '$_POST[id]'");
 header('Location:admin.php');
