1cd1d5f186f1210dc22f6f8773657cf6f0ed0a69
Dimitar.Petroww <Mitko66692@abv.bg>
2021-04-01 14:52:24 +0300

    update

--- index.php
+++ index.php
@@ -9,10 +9,30 @@
 
 	<link rel="stylesheet" href="./css/master.css">
 	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<style>
 		h1{
 			display: inline-block;
 			margin: 10px;
+		}
+		form.searchForm input[type=text] {
+  			padding: 10px;
+  			font-size: 17px;
+  			border: 1px solid grey;
+  			float: left;
+  			width: 80%;
+  			background: #f1f1f1;
+		}
+		form.searchForm button {	
+  			float: left;
+  			width: 20%;
+  			padding: 10px;
+  			background: #2196F3;
+  			color: white;
+  			font-size: 17px;
+  			border: 1px solid grey;
+  			border-left: none; /* Prevent double borders */
+  			cursor: pointer;
 		}
 	</style>
 </head>
@@ -21,11 +41,25 @@
 		<header class="site-header">
 			<h1 class="site-title"><a href="./index.php">Job Offers</a></h1>
 			<h1 class="site-title"><a href="./create.php">Create Job</a></h1>
+			<form class="searchForm" action="./index.php" method="post">
+  				<input type="text" placeholder="Search.." name="search">
+  				<button type="submit"><i class="fa fa-search"></i></button>
+			</form>
 		</header>
         <?php
         echo '<ul class="jobs-listing">';
         $db = new mysqli("localhost", "root", "", "devriX");
-        $result = $db->query("SELECT * FROM jobs");
+		$result;
+		if(isset($_POST["search"])) {
+			$search = $_POST["search"];
+			$result = $db->query("SELECT * FROM jobs 
+            WHERE 
+            title LIKE '%{$search}%'
+            OR
+            company LIKE '%{$search}%'");	
+		}else {
+			$result = $db->query("SELECT * FROM jobs");	
+		}
         $jobs = $result->fetch_all(MYSQLI_ASSOC);
         foreach ($jobs as $job) {
             echo "<li class=job-card>
